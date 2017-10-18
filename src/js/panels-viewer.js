cf7917e3e5357d7d8502da40fca84bee72a52bb1
Simon Pioli <simon@simonpioli.com>
2017-10-18 14:42:45 +0100

    Correct Language selection

--- src/js/panels-viewer.js
+++ src/js/panels-viewer.js
@@ -78,7 +78,7 @@ var panelsViewer = {
 					var e = new XMLHttpRequest();
 					e.onload = (function (i, panels, patternData, iframeRequest) {
 						return function () {
-							prismedContent    = Prism.highlight(this.responseText, Prism.languages[PrismLanguages.get(panels[i].language)]);
+							prismedContent    = Prism.highlight(this.responseText, Prism.languages[panels[i].language]);
 							template          = document.getElementById(panels[i].templateID);
 							templateCompiled  = Hogan.compile(template.innerHTML);
 							templateRendered  = templateCompiled.render({
